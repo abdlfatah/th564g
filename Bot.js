0fae5f1f2e259e3449dfc35d54b728d758444d9d
abdlfatah <45146141+abdlfatah@users.noreply.github.com>
2019-01-05 02:48:38 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,30 +1,27 @@
- const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('message', message => {
-	    var prefix = "-";
+     const Discord = require("discord.js");
+     const client = new Discord.Client()
+     client.on('message', message => {
+        var prefix = "-";
               if(!message.channel.guild) return;
     if(message.content.startsWith(prefix + 'bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
     let copy = "Super Bot";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
     .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-    
+    .then(() =>msg.react('✅'))   
     let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
     let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-    
     let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
     let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
+    reaction1.on("collect", r => {
     message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
     message.guild.members.forEach(m => {
-  
-  var bc = new
-       Discord.RichEmbed()
+    var bc = new
+    Discord.RichEmbed()
        .setColor('RANDOM')
        .setTitle('Broadcast')
        .addField('سيرفر', message.guild.name)
@@ -43,4 +40,5 @@ client.on('message', message => {
     })
     }
     });
-    client.login(process.env.BOT_TOKEN);//
+
+client.login(process.env.BOT_TOKEN);// 
